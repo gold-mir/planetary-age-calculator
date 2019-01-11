3cfd6dd17c96259c0d43fd4b1465f43612b0a9a0
gold-mir <miranda.keith.42@gmail.com>
2019-01-11 13:18:41 -0800

    passing initial tests

--- src/ageCalc.js
+++ src/ageCalc.js
@@ -1,6 +1,16 @@
 export class AgeCalculator {
   //creates a new AgeCalculator with a specified year length in earth years
-  constructor(function yearLength){
-
+  constructor(yearLength, date = new Date()){
+    this.yearLength = yearLength;
+    this.date = date.getTime();
   }
+
+  getYearLength(){
+    return this.yearLength;
+  }
+
+  getDate(){
+    return this.date;
+  }
+
 }
