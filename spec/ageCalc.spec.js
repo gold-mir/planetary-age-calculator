431cff92fe2002b27a0fd929f50390216e44aa20
gold-mir <miranda.keith.42@gmail.com>
2019-01-11 13:31:37 -0800

    add test for getYearsFrom method

--- spec/ageCalc.spec.js
+++ spec/ageCalc.spec.js
@@ -1,19 +1,24 @@
 import { AgeCalculator } from './../src/ageCalc.js';
 
 describe('Age Calculator', () => {
-  let vogon;
+
+  let planetx;
+  let newYears;
 
   beforeEach(() => {
-    vogon = new AgeCalculator(2);
+    newYears = new Date("2019-01-01");
+    planetx = new AgeCalculator(0.5, newYears);
   });
 
   it('should have a constructor that creates a new AgeCalculator object with a given year length', () => {
-    expect(vogon.getYearLength()).toEqual(2);
+    expect(planetx.getYearLength()).toEqual(0.5);
   });
 
   it('should have an optional parameter that allows you to set the current date, and should otherwise default to now', () => {
-    let newYears = new Date("2019-01-01");
-    let planetx = new AgeCalculator(0.5, newYears);
     expect(planetx.getDate()).toEqual(newYears.getTime());
+  });
+
+  it("should have a getYearsFrom method that returns the number of years from an inputted date to the calculator's date in modified years", () => {
+    expect(planetx.getYearsFrom('2018-01-01')).toEqual(2);
   });
 });
